369dc23460a966820aaecfe3fb0ad5568b04bb1c
Tamir S. Ferreira <bigtam_rj@yahoo.com.br>
2023-05-27 23:24:42 -0300

    feat: first page skeleton in angular

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,17 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css'],
+  /* styles: [
+    `
+      app-big-card {
+        border: 1px solid yellow;
+        background: red;
+      }
+    `,
+  ], */
 })
-export class BigCardComponent {
-
-}
+export class BigCardComponent {}
